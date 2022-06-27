b97259504c510ca99cb130dbc74b41b3b2476651
Heydilv <heydijaritza98@hotmail.com>
2022-06-27 15:11:12 -0500

    Com

--- app/src/main/java/com/example/tarea2/MainActivity.kt
+++ app/src/main/java/com/example/tarea2/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.tarea2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //hola hola
+
+
     }
+
+
+    fun CosultaV(view: View){
+        val textView = findViewById<TextView>(R.id.textId)
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://gorest.co.in/public/v1/users"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                textView.text = "Response is: ${response.toString()}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+
+
+
+
+    }
+
+
 }
